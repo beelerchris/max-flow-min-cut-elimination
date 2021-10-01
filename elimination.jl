5a06ed039367899c222e7fb5cf09c0c9dd9b6edd
Chris Beeler <cbeeler@dal.ca>
2021-10-01 16:06:50 -0400

    Added finalized project code

---
+++ elimination.jl
@@ -0,0 +1,73 @@
+include("graph_functions.jl")
+
+# Name of the file containing the division information
+division = "division_1.txt"
+
+# Parse divinsion information into useful information
+teams, games, A = read_division(division)
+
+# Get the number of teams
+n = size(teams)[1]
+
+for i in 1:n
+    # Create a graph and capacity matrix for Team i.
+    flow_graph, capacity_matrix = define_graph(games, A, i, 1000)
+    n_nodes = size(capacity_matrix)[1]
+    teams_1 = collect(Iterators.flatten((1:(i-1), (i+1):n)))
+    elim_check = true
+    # Check if any team already has more wins than Team i can attain.
+    all_wins = collect(games[j][1] for j in Iterators.flatten((1:(i-1), (i+1):n)))
+    if (games[i][1] + games[i][3]) < max(all_wins...)
+        elim_check = false
+        win_max = maximum(all_wins)
+        win_team = teams_1[findall(x->x==win_max, all_wins)[1]]
+        println(teams[i] * " is eliminated.")
+        println("They can win at most " * string(games[i][1]) * " + " * string(games[i][3]) * " = " * string(games[i][1] + games[i][3]) * " games.")
+        println(teams[win_team] * " has already won " * string(win_max) * " games.")
+        println(" ")
+    end
+
+    # If not, find the min-cut of Team i's graph.
+    if elim_check
+        part1, part2, v = LightGraphsFlows.mincut(flow_graph, 1, n_nodes, capacity_matrix, BoykovKolmogorovAlgorithm())
+        cut_teams = []
+        # Check which teams are in part 1 (the source side).
+        for j in 1:(size(part1)[1] - 1)
+            if part1[j+1] > (n_nodes - n)
+                team_ind = teams_1[part1[j+1] + n - n_nodes]
+                cut_team = (teams[team_ind], team_ind)
+                cut_teams = [cut_teams; cut_team]
+            end
+        end
+
+        # Create the certificate of elimination if required.
+        if size(part1)[1] > 1
+            println(teams[i] * " is eliminated.")
+            println("They can win at most " * string(games[i][1]) * " + " * string(games[i][3]) * " = " * string(games[i][1] + games[i][3]) * " games.")
+            reason = cut_teams[1][1]
+            local wins = games[cut_teams[1][2]][1]
+            for j in 2:size(cut_teams)[1]
+                reason = reason * " and " * cut_teams[j][1]
+                wins += games[cut_teams[j][2]][1]
+            end
+            println(reason * " have won a total of " * string(wins) * " games.")
+            local n_matches = 0
+            for j in 1:size(cut_teams)[1]
+                for k in (j+1):size(cut_teams)[1]
+                    n_matches += A[cut_teams[j][2], cut_teams[k][2]]
+                end
+            end
+            # Determine if we round the average wins up or not.
+            rem = (wins + n_matches) / size(cut_teams)[1] - (wins + n_matches) ÷ size(cut_teams)[1]
+            if rem > 1e-9
+                rem2 = 1
+            else
+                rem2 = 0
+            end
+            println("They play each other " * string(n_matches) * " times.")
+            println("On average, each of the teams in this group wins " * string(wins + n_matches) * " / " * string(size(cut_teams)[1]) * " = " * string(round((wins + n_matches) / size(cut_teams)[1], digits=2)) * " games.")
+            println("Therefore one team will win at least " * string((wins + n_matches) ÷ size(cut_teams)[1] + rem2) * " games.")
+            println(" ")
+        end
+    end
+end
---
+++ quidditch.jl
@@ -0,0 +1,76 @@
+using Random
+
+# The names of all possible teams.
+all_teams = ["Appleby_Arrows      ",
+             "Ballycastle_Bats    ",
+             "Caerphilly_Catapults",
+             "Chudley_Cannons     ",
+             "Falmouth_Falcons    ",
+             "Holyhead_Harpies    ",
+             "Kenmare_Kestrels    ",
+             "Montrose_Magpies    ",
+             "Pride_of_Portree    ",
+             "Puddlemere_United   ",
+             "Tutshill_Tornados   ",
+             "Wigtown_Wanderers   ",
+             "Wimbourne_Wasps     "]
+
+# The total number of teams included in the division. (2 <= n <= 13)
+n = 13
+
+# Randomly select n teams from the list of 13.
+teams = shuffle(all_teams)[1:n]
+
+# Initialize the schedule matrix for 2 matches per match-up.
+A = zeros(Int, n, n)
+for i in 1:n
+    for j in 1:n
+        if i != j
+            A[i, j] = 2
+        end
+    end
+end
+
+# Randomly assign a skill level to each team.
+chances = rand(n)
+
+# Initialize the wins and losses of each team at 0.
+wins = zeros(Int, n)
+losses = zeros(Int, n)
+
+# Simulate an outcome for each match and store the division standings.
+count = 0
+while sum(A) > 1e-9
+    global count += 1
+    # Randomly select a match from the remaining unplayed matches.
+    games = findall(x->x>1e-9, A)
+    game = rand(1:size(games)[1])
+    t1 = games[game][1]
+    t2 = games[game][2]
+    # Determine the probability team 1 wins the match and decide a winner.
+    prob = chances[t1] / (chances[t1] + chances[t2])
+    r = rand()
+    if r <= prob
+        wins[t1] += 1
+        losses[t2] += 1
+    else
+        wins[t2] += 1
+        losses[t1] += 1
+    end
+    # Update the remaining match schedule.
+    A[t1, t2] -= 1
+    A[t2, t1] -= 1
+
+    # Generate the division standings and store them in a text file.
+    div = "# Team               Win Loss Left\n--------------------------------------------------\n"
+    for i in 1:n
+        div = div * string(i) * " " * teams[i] * " " * string(wins[i]) * "    " * string(losses[i]) * "   " * string(sum(A[i, :])) * " "
+        for j in 1:n
+            div = div * string(A[i, j]) * " "
+        end
+        div = div * "\n"
+    end
+    open("./quidditch_div/division_quid_" * string(count) * ".txt", "w") do text_file
+        write(text_file, div)
+    end
+end
